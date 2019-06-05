252baf9afa16ef8d4ad1c8de84d0cb7136cb2aac
mesgu2 <mesgu2@email.com>
2019-06-05 14:02:13 -0700

    initialized test class for user class

---
+++ src/test/java/user/userTest.java
@@ -0,0 +1,24 @@
+package user;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class userTest {
+
+    @Test
+    public void getUsername() {
+    }
+
+    @Test
+    public void getPassword() {
+    }
+
+    @Test
+    public void getMd5() {
+    }
+
+    @Test
+    public void display() {
+    }
+}
